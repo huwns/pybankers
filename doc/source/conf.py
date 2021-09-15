da9be449b736b67eea5ef57d192902bb5dd34fdf
huwns <test@gmail.com>
2021-09-15 21:31:43 +0900

    add useful extentions

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -33,6 +33,8 @@ release = '2021/1/6'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.graphviz',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.inheritance_diagram',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
